bb1f36752e3dae6e9ca59b0f8f86457daa3527f0
blacpeaz <chidiigweonu@gmail.com>
2023-06-04 14:04:43 +0300

    added Netlify link

--- src/App.js
+++ src/App.js
@@ -14,6 +14,14 @@ export default function App() {
             rel="noreferrer"
           >
             open-sourced on GitHub
+          </a>{" "}
+          and on{" "}
+          <a
+            href="https://curious-liger-10c9d1.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
           </a>
         </footer>
       </div>
